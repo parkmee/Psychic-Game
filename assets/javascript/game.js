0b2f27d477905000658d6bc3a86f7bcd1467fab2
MMPark <parkmee@gmail.com>
2018-08-29 17:17:51 -0400

    week 3 hw psychic wip

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,9 @@
 var letter = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 
 // variables to track number of wins, losses, and remaining guesses
-var wins = 0;
-var losses = 0;
-var guesses = 10;
+var win = 0;
+var loss = 0;
+var guess = 10;
 
 // variables to hold references to the places in the html where text will be printed
 var directionsText = document.getElementById("directions-text");
@@ -12,11 +12,42 @@ var lossesText = document.getElementById("losses-text");
 var guessesLeft = document.getElementById("guesses-left");
 var userGuessesText = document.getElementById("userGuesses-text");
 
-// run when user presses key
+// user selects key
 document.onkeyup = function(event) {
 
-    // determines which key was pressed
+var randomLetter;
+
+    ///if (guess === 10) {
+        randomLetter = letter[Math.floor(Math.random() * letter.length)];
+    console.log(randomLetter);
+    ///}
+
+    // selected key recorded
     var userGuess = event.key;
+
+    // outcomes
+    if (userGuess.toLowerCase() === randomLetter) {
+    win++;
+
+    } else {
+        guess--;
+    }
+    
+    directionsText.textContent = "";
+    userGuessesText.append(userGuess);
+
+    if (guess === 0) {
+        directionsText.textContent = "Guess what letter I'm thinking of..."
+        loss++;
+        guess = 10;
+        userGuessesText.textContent = "";
+    } 
+                
+
+    //
+    winsText.textContent = "Wins: " + win;
+    lossesText.textContent = "Losses: " + loss;
+    guessesLeft.textContent = "Guesses left: " + guess;
 
 
 }
