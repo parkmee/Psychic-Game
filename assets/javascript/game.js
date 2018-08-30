ab1bbfa753dc01f8b765392cc215a38cd3c8a862
MMPark <parkmee@gmail.com>
2018-08-29 20:25:24 -0400

    week 3 hw psychic wip

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,7 +3,7 @@ var letter = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q
 // variables to track number of wins, losses, and remaining guesses
 var win = 0;
 var loss = 0;
-var guess = 10;
+var guess = 9;
 
 // variables to hold references to the places in the html where text will be printed
 var directionsText = document.getElementById("directions-text");
@@ -15,39 +15,55 @@ var userGuessesText = document.getElementById("userGuesses-text");
 // user selects key
 document.onkeyup = function(event) {
 
-var randomLetter;
-
-    ///if (guess === 10) {
-        randomLetter = letter[Math.floor(Math.random() * letter.length)];
+    var randomLetter = letter[Math.floor(Math.random() * letter.length)];
     console.log(randomLetter);
-    ///}
+
+    // enter code here to have the random letter only selected once per game
 
     // selected key recorded
     var userGuess = event.key;
-
-    // outcomes
+    
+    // check to see if guess is a letter
+      
+    // user guess equals random letter, add one to win score
     if (userGuess.toLowerCase() === randomLetter) {
-    win++;
+        // increase win count
+        win++;
+        // reset guesses count
+        guesses = 9;
+        // clear list of guesses
+        userGuessesText.textContent = "";
 
+    // decrement guess count for each mismatch
     } else {
         guess--;
     }
     
-    directionsText.textContent = "";
-    userGuessesText.append(userGuess);
+    // log user guesses
+    if (guess === 8) {
+    userGuessesText.textContent = "Your guesses: " + userGuess;
+    } else {
+        userGuessesText.append(", " + userGuess);
+    }
 
+    // reset if guess count equals 0
     if (guess === 0) {
-        directionsText.textContent = "Guess what letter I'm thinking of..."
+
+        // increase loss count
         loss++;
-        guess = 10;
+
+        // reset guess count to 9
+        guess = 9;
+
+        // clear list of guesses
         userGuessesText.textContent = "";
     } 
-                
+    
 
-    //
+    // print number of wins, losses, and guesses
+    directionsText.textContent = "Guess what letter I'm thinking of...";
     winsText.textContent = "Wins: " + win;
     lossesText.textContent = "Losses: " + loss;
     guessesLeft.textContent = "Guesses left: " + guess;
-
 
 }
